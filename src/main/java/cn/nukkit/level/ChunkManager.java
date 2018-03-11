dcee3693f35f9fb9a9a3fbfc0aeaec1f03177692
kuro-123 <noripapion@gmail.com>
2018-03-11 15:09:28 +0900

    初期コミット

---
+++ src/main/java/cn/nukkit/level/ChunkManager.java
@@ -0,0 +1,28 @@
+package cn.nukkit.level;
+
+import cn.nukkit.level.format.generic.BaseFullChunk;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public interface ChunkManager {
+
+    int getBlockIdAt(int x, int y, int z);
+
+    void setBlockFullIdAt(int x, int y, int z, int fullId);
+
+    void setBlockIdAt(int x, int y, int z, int id);
+
+    int getBlockDataAt(int x, int y, int z);
+
+    void setBlockDataAt(int x, int y, int z, int data);
+
+    BaseFullChunk getChunk(int chunkX, int chunkZ);
+
+    void setChunk(int chunkX, int chunkZ);
+
+    void setChunk(int chunkX, int chunkZ, BaseFullChunk chunk);
+
+    long getSeed();
+}
